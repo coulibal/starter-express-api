511cfee3d0d760b71bccc082bf41572cf9a48383
Franck Coulibaly <44041782+coulibal@users.noreply.github.com>
2022-09-18 20:09:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const { MongoClient} = require('mongodb');
+const express = require('express');
+const app = express();
+
+const uri = process.env.MONGO_CONNECTION_STRING;
+const client = new MongoClient(uri);
+
+app.get("/items/:my_item", async (req, res) => {
+    let my_item = req.params.my_item;
+    let item = await client.db("my_db")
+                .collection("my_collection")
+                .findOne({my_item: my_item})
+
+    return res.json(item)
 })
-app.listen(process.env.PORT || 3000)
+
+client.connect(err => {
+    if(err){ console.error(err); return false;}
+    // connection to mongo is successful, listen for requests
+    app.listen(3000, () => {
+        console.log("listening for requests");
+    })
+});
